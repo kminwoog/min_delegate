c058aa5e968de6dc2ff396a4edde848b0ded0520
kminwoog <kminwoog@gmail.com>
2018-04-10 22:19:53 +0900

    #00 project new initial commits

---
+++ lib/min_delegate.ex
@@ -0,0 +1,18 @@
+defmodule MinDelegate do
+  @moduledoc """
+  Documentation for MinDelegate.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MinDelegate.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/min_delegate_test.exs
@@ -0,0 +1,8 @@
+defmodule MinDelegateTest do
+  use ExUnit.Case
+  doctest MinDelegate
+
+  test "greets the world" do
+    assert MinDelegate.hello() == :world
+  end
+end
