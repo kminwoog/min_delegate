8259e62bff3c246beb2f66dd5cba08fed5037710
kminwoog <kminwoog@gmail.com>
2018-04-18 00:28:41 +0900

    #00 add .travis.yml for ci

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule MinDelegate.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
@@ -39,7 +40,7 @@ defmodule MinDelegate.MixProject do
       # This option is only needed when you don't want to use the OTP application name
       name: "min_delegate",
       # These are the default files included in the package
-      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      files: ["lib", "test", "config", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["kim min woog"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/kminwoog/min_delegate"}
